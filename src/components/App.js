bff7ee29bcf89f143ac6625b514b9d0ee7710562
howjeejyee <146084645+howjeejyee@users.noreply.github.com>
2023-11-14 11:11:47 +0800

    4th commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SkillList from './SkillList';
 import NewSkillForm from './NewSkillForm';
 import './App.css';
 
 const skills = [
-  { name: "HTML", level: 5 },
-  { name: "CSS", level: 3 },
-  { name: "JavaScript", level: 4 },
-  { name: "Python", level: 2 },
+  { name: 'HTML', level: 5 },
+  { name: 'CSS', level: 3 },
+  { name: 'JavaScript', level: 4 },
+  { name: 'Python', level: 2 },
 ];
 
 const App = () => {
+  const [skill, setSkill] = useState(skills);
+
   return (
     <div>
       <h1>React Dev Skills</h1>
-      <SkillList skills={skills} />
+      <SkillList skills={skill} />
       <hr />
       <NewSkillForm />
     </div>
@@ -22,5 +24,3 @@ const App = () => {
 };
 
 export default App;
-
-
